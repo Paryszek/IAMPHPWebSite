f870b9b47363ed0ff77a6ae2479a7e5ad10e0437
Paryszek <michal.parysz@yahoo.com>
2018-01-23 01:15:00 +0100

    sql

--- sql/projekt.sql
+++ sql/projekt.sql
@@ -1,15 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.4
+-- version 4.6.5.2
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost
--- Czas generowania: 19 Sty 2018, 00:53
--- Wersja serwera: 10.1.28-MariaDB
--- Wersja PHP: 5.6.32
+-- Host: 127.0.0.1
+-- Czas generowania: 23 Sty 2018, 01:14
+-- Wersja serwera: 10.1.21-MariaDB
+-- Wersja PHP: 5.6.30
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -25,13 +23,31 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Struktura tabeli dla tabeli `cities`
+-- Struktura tabeli dla tabeli `comments`
 --
 
-CREATE TABLE `cities` (
-  `city_id` int(11) NOT NULL,
-  `city_name` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+CREATE TABLE `comments` (
+  `comment_id` int(11) NOT NULL,
+  `text` varchar(300) COLLATE utf8_polish_ci NOT NULL,
+  `rate` int(11) NOT NULL,
+  `date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `author` varchar(50) COLLATE utf8_polish_ci NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `post_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
+
+--
+-- Zrzut danych tabeli `comments`
+--
+
+INSERT INTO `comments` (`comment_id`, `text`, `rate`, `date`, `author`, `user_id`, `post_id`) VALUES
+(1, 'qweqweqweqw', 0, '2018-01-22 23:24:04', 'Mariusz Kowalski', 1, 6),
+(2, 'werweqrwerwer', 0, '2018-01-22 23:41:46', 'Mariusz Kowalski', 1, 6),
+(3, 'eqweqw', 0, '2018-01-22 23:52:37', 'Mariusz Kowalski', 1, 6),
+(4, 'qweqweqwe', 0, '2018-01-22 23:53:00', 'Maciek Nowak', 3, 7),
+(5, 'qweqweqweq', 0, '2018-01-22 23:53:07', 'Maciek Nowak', 3, 6),
+(6, 'rqweqweqwe', 0, '2018-01-23 00:47:46', 'Maciek Nowak', 3, 9),
+(7, 'qweqweqw', 0, '2018-01-23 00:47:57', 'Maciek Nowak', 3, 9);
 
 -- --------------------------------------------------------
 
@@ -41,21 +57,30 @@ CREATE TABLE `cities` (
 
 CREATE TABLE `posts` (
   `post_id` int(11) NOT NULL,
-  `text` varchar(2000) NOT NULL,
-  `author` varchar(50) NOT NULL,
-  `rate` int(5) NOT NULL
+  `title` varchar(50) CHARACTER SET utf8 COLLATE utf8_polish_ci NOT NULL,
+  `text` varchar(2000) CHARACTER SET utf8 COLLATE utf8_polish_ci NOT NULL,
+  `author` varchar(50) CHARACTER SET utf8 COLLATE utf8_polish_ci NOT NULL,
+  `region_name` varchar(40) CHARACTER SET utf8 COLLATE utf8_polish_ci NOT NULL,
+  `region_type` int(11) NOT NULL,
+  `rate` int(5) NOT NULL DEFAULT '0',
+  `date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `user_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- --------------------------------------------------------
-
 --
--- Struktura tabeli dla tabeli `regions`
+-- Zrzut danych tabeli `posts`
 --
 
-CREATE TABLE `regions` (
-  `region_id` int(11) NOT NULL,
-  `region_name` varchar(30) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+INSERT INTO `posts` (`post_id`, `title`, `text`, `author`, `region_name`, `region_type`, `rate`, `date`, `user_id`) VALUES
+(1, 'asdasdas', 'asdasdwqeqweqweqweqw', 'Maciek Nowak', 'Wojew&oacute;dztwo Ł&oacute;dzkie', 2, 0, '2018-01-22 01:50:02', 3),
+(2, 'qewewqe', 'qweqweqweqwe', 'Maciek Nowak', 'Polska', 1, 0, '2018-01-22 01:50:02', 3),
+(3, 'qweqwe', 'qweqwe', 'Mariusz Kowalski', 'Polska', 1, 0, '2018-01-22 01:59:45', 1),
+(4, 'wqeqweqweqwe', 'qweqweqweqweqw', 'Maciek Nowak', 'Polska', 1, 0, '2018-01-22 15:20:32', 3),
+(5, 'qweqweqwe', 'qweqwe', 'Mariusz Kowalski', 'Polska', 1, 0, '2018-01-22 22:28:51', 1),
+(6, 'dasdasd', 'sadasdsa', 'Mariusz Kowalski', 'Łódź Wschód', 2, 0, '2018-01-22 22:40:48', 1),
+(7, 'sdasdasd', 'asdasdasd', 'Mariusz Kowalski', 'Polska', 1, 0, '2018-01-22 23:52:52', 1),
+(8, 'weqweqwe', 'qweqweqwe', 'Maciek Nowak', 'Polska', 1, 0, '2018-01-23 00:36:19', 3),
+(9, 'wqeqweqwe', 'sadqeqwewqeqwe', 'Maciek Nowak', 'Łódź', 3, 0, '2018-01-23 00:37:38', 3);
 
 -- --------------------------------------------------------
 
@@ -81,38 +106,24 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`user_id`, `Login`, `E-mail`, `Password`, `First`, `Last`, `Age`, `City`, `Region`, `Country`) VALUES
-(1, 'mariusz_44', '', 'admin', 'Mariusz', 'Kowalski', 24, 'Koluszki', 'Łódź Wschód', 'Polska'),
-(2, 'mariusz', 'mariusz@o2.pl', 'admin', 'Mariusz', 'Kowalski', 28, 'Koluszki', 'Ł&oacute;dź Wsch&oacute;d', 'Polska'),
-(3, 'maciek_44', 'maciek@o2.pl', 'maciek', 'Maciek', 'Nowak', 28, 'Ł&oacute;dź', 'Wojew&oacute;dztwo Ł&oacute;dzkie', 'Polska'),
-(4, 'asd', 'asd', 'asd', 'asd', 'asd', 22, 'asd', 'asd', 'asd'),
-(5, 'aa', 'aa', 'aa', 'aa', 'aa', 22, 'aa', 'aa', 'aa'),
-(6, 'bb', 'bb', 'bb', 'bb', 'bb', 22, 'bb', 'bb', 'bb'),
-(7, 'gg', 'gg', 'gg', 'gg', 'gg', 22, 'gg', 'gg', 'gg'),
-(8, 'ddd', 'ddd', '123', 'ddd', 'dd', 22, 'dd', 'dd', 'dd'),
-(9, '', '', '', '', '', 0, '', '', ''),
-(10, '', '', '', '', '', 0, '', '', '');
+(1, 'mariusz_44', 'mariusz_44@gmail.com', 'admin', 'Mariusz', 'Kowalski', 24, 'Koluszki', 'Łódź Wschód', 'Polska'),
+(3, 'maciek_44', 'maciek@o2.pl', 'maciek', 'Maciek', 'Nowak', 28, 'Łódź', 'Łódź Wschód', 'Polska');
 
 --
 -- Indeksy dla zrzutów tabel
 --
 
 --
--- Indexes for table `cities`
+-- Indexes for table `comments`
 --
-ALTER TABLE `cities`
-  ADD PRIMARY KEY (`city_id`);
+ALTER TABLE `comments`
+  ADD PRIMARY KEY (`comment_id`);
 
 --
 -- Indexes for table `posts`
 --
 ALTER TABLE `posts`
   ADD PRIMARY KEY (`post_id`);
-
---
--- Indexes for table `regions`
---
-ALTER TABLE `regions`
-  ADD PRIMARY KEY (`region_id`);
 
 --
 -- Indexes for table `users`
@@ -125,30 +136,20 @@ ALTER TABLE `users`
 --
 
 --
--- AUTO_INCREMENT dla tabeli `cities`
+-- AUTO_INCREMENT dla tabeli `comments`
 --
-ALTER TABLE `cities`
-  MODIFY `city_id` int(11) NOT NULL AUTO_INCREMENT;
-
+ALTER TABLE `comments`
+  MODIFY `comment_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 --
 -- AUTO_INCREMENT dla tabeli `posts`
 --
 ALTER TABLE `posts`
-  MODIFY `post_id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT dla tabeli `regions`
---
-ALTER TABLE `regions`
-  MODIFY `region_id` int(11) NOT NULL AUTO_INCREMENT;
-
+  MODIFY `post_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 --
 -- AUTO_INCREMENT dla tabeli `users`
 --
 ALTER TABLE `users`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
-COMMIT;
-
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
